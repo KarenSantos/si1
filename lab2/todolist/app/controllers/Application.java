138520a5d746599dd84eb04cbb04454257f00f17
KarenSantos <karensantosrocha@gmail.com>
2013-11-21 23:54:59 -0300

    pagina de tasks criada

--- lab2/todolist/app/controllers/Application.java
+++ lab2/todolist/app/controllers/Application.java
@@ -2,17 +2,21 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import models.*;
 
 import views.html.*;
 
 public class Application extends Controller {
+
+	static Form<Task> taskForm = Form.form(Task.class);
 
     public static Result index() {
         return redirect(routes.Application.tasks());
     }
     
     public static Result tasks() {
-    	return TODO;
+    	return ok(views.html.index.render(Task.all(), taskForm));
     }
     
     public static Result newTask() {
